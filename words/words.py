51ae1917bf0e68f963cc7a9a98d2ec384b2f3517
Henrik Peters <henrik.peters.98@web.de>
2018-12-18 09:07:25 +0100

    added another try for word 17. no success

--- words/words.py
+++ words/words.py
@@ -1,5 +1,6 @@
 import itertools
 import numpy as np
+import sympy
 import time
 
 with open('deutsch.txt', 'r') as file:
@@ -221,57 +222,127 @@ def input(word):
 ##=======================================================
 ##-------------------------------------------------------
 
-start = time.time()
-print("word 1")
-calc_set(*input("freenst"))
-print("word 2")
-calc_set(*input("treiten"))
-print("word 3")
-calc_set(*input("adinost"))
-print("word 4")
-calc_set(*input("addikant"))
-print("word 5")
-calc_set(*input("prionator"))
-print("word 6")
-calc_set(*input("teotelti"))
-print("word 7")
-calc_set(*input("grereist"))
-print("word 8")
-calc_set(*input("halzgnu"))
-print("word 9")
-calc_set(*input("keidroma"))
-print("word 10")
-calc_set(*input("wandkreh"))
-print("word 11")
-calc_set(*input("rindteig"))
-print("word 12")
-calc_set(*input("pechobrot"))
-print("word 13")
-calc_set(*input("strinkdich"))
-print("word 14")
-calc_set(*input("nachemtritt"))
-print("word 15")
-calc_set(*input("daergenle"))
-print("word 16")
-calc_set(*input("fundheutert"))
+def perms(s):
+    if(len(s)==1): return [s]
+    result=[]
+    for i,v in enumerate(s):
+        result += [v+p for p in perms(s[:i]+s[i+1:])]
+    return result
+
+##-------------------------------------------------------
+##=======================================================
+##-------------------------------------------------------
+
+def calc_np_multiset(a, b, c, d, e='', f='', g='', h='', i='', j='', k='', l=''):
+    word = [a, b, c, d]
+    if e != '':
+        word.append(e)
+    if f != '':
+        word.append(f)
+    if g != '':
+        word.append(g)
+    if h != '':
+        word.append(h)
+    if i != '':
+        word.append(i)
+    if j != '':
+        word.append(j)
+    if k != '':
+        word.append(k)
+    if l != '':
+        word.append(l)
+
+    word = np.asarray(word)
+    solutions = set()
+    for p in sympy.utilities.iterables.multiset_permutations(word):
+        solutions.add(''.join(p))
+
+    print(len(solutions))
+    final_solutions = set()
+    found_flag = False
+    for i, solution in enumerate(solutions):
+        if solution in german_words_set:
+            found_flag = True
+            final_solutions.add(solution)
+        if solution in german_words_set2:
+            found_flag = True
+            final_solutions.add(solution)
+        if solution in german_words_set3:
+            found_flag = True
+            final_solutions.add(solution)
+        #if i%100 == 0:
+            #print("Check word "+str(i)+" of "+str(len(solutions))+"\r")
+
+    if found_flag == True:
+        print("\n" + "Finished Search successfully")
+        print(final_solutions)
+        print("----------------------------")
+    else:
+        print("\n" + "no matching words found")
+        print("----------------------------")
+
+def input(word):
+    word = list(word.upper())
+    dif = 12-len(word)
+    for i in range(dif):
+        word.append('')
+    return word[0], word[1], word[2], word[3], word[4], word[5], word[6], word[7], word[8], word[9], word[10], word[11]
+
+##-------------------------------------------------------
+##=======================================================
+##-------------------------------------------------------
+
+#start = time.time()
+#print("word 1")
+#calc_set(*input("freenst"))
+#print("word 2")
+#calc_set(*input("treiten"))
+#print("word 3")
+#calc_set(*input("adinost"))
+#print("word 4")
+#calc_set(*input("addikant"))
+#print("word 5")
+#calc_set(*input("prionator"))
+#print("word 6")
+#calc_set(*input("teotelti"))
+#print("word 7")
+#calc_set(*input("grereist"))
+#print("word 8")
+#calc_set(*input("halzgnu"))
+#print("word 9")
+#calc_set(*input("keidroma"))
+#print("word 10")
+#calc_set(*input("wandkreh"))
+#print("word 11")
+#calc_set(*input("rindteig"))
+#print("word 12")
+#calc_set(*input("pechobrot"))
+#print("word 13")
+#calc_set(*input("strinkdich"))
+#print("word 14")
+#calc_set(*input("nachemtritt"))
+#print("word 15")
+#calc_set(*input("daergenle"))
+#print("word 16")
+#calc_set(*input("fundheutert"))
 print("word 17")
 print("toohard")
 print("-------------------------")
-#calc_set(*input("krechsaftweg"))
-print("word 18")
-calc_set(*input("knechges"))
-print("word 19")
-#calc_set(*input("sunnebohbton"))
-print("toohard")
-print("-------------------------")
-print("word 20")
-calc_set(*input("helgusteil"))
-print("word 21")
-calc_set(*input("sachpinsem"))
-print("word 22")
-calc_set(*input("amorkeid"))
-print("word 23")
-calc_set(*input("freinzieger"))
-print("word 24")
-calc_set(*input("bananenmut"))
-print(time.time() - start)
+calc_np_multiset(*input("krechsaftweg"))
+#print("word 18")
+#calc_set(*input("knechges"))
+#print("word 19")
+##calc_set(*input("sunnebohbton"))
+#print("toohard")
+#print("-------------------------")
+#print("word 20")
+#calc_set(*input("helgusteil"))
+#print("word 21")
+#calc_set(*input("sachpinsem"))
+#print("word 22")
+#calc_set(*input("amorkeid"))
+#print("word 23")
+#calc_set(*input("freinzieger"))
+#print("word 24")
+#calc_set(*input("bananenmut"))
+#print(time.time() - start)
